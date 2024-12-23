135b9c7e4a12c09054993655ffac4f8dc3fab6e4
Ankur Gupta <ankur2115gupta29@gmail.com>
2024-12-23 15:52:12 +0530

    backup 2024-12-23 15:52:12

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark',
+extensions = ['myst_parser',
     'sphinx_rtd_theme', ]
 
 templates_path = ['_templates']
