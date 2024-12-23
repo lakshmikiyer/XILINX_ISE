6264fa0b9ba31101c19fa00a13d1e172f43f9c18
Ankur Gupta <ankur2115gupta29@gmail.com>
2024-12-23 17:07:30 +0530

    backup 2024-12-23 17:07:30

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme',"myst_parser",]
+extensions = ['sphinx_rtd_theme',"myst_parser","sphinx_design",]
 
 templates_path = ['_templates']
 exclude_patterns = []
